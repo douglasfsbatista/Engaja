bd3e52bfcaa83626e7f9e40c96504dac87bd4995
Douglas Filipe <batista.douglasfs@gmail.com>
2025-09-04 10:21:40 -0300

    fix: inserção das permissões de acesso nas rotas

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth','role:administrador'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -26,7 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/eventos/{evento}/inscritos', [\App\Http\Controllers\InscricaoController::class, 'inscritos'])->name('inscricoes.inscritos');
 });
 
-Route::middleware(['auth','verified'])->group(function () {
+Route::middleware(['auth','role:administrador'])->group(function () {
     Route::resource('eventos', \App\Http\Controllers\EventoController::class);
 });
 
